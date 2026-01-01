6299460184d9ade841a1e44e8aba35d0458bba60
Abhinay yadav <abhinayy48@gmail.com>
2026-01-01 21:32:19 +0530

    Reorder dotenv configuration and express import for clarity

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-import express from "express";
 import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
 import connectDB from "./config/db.js";
 import "./services/cron.service.js";
 
 
 
-dotenv.config();
+
 connectDB();
 
 const app = express();
