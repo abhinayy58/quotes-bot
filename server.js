86e5c2f64f705a14d6219cc523ff1226583f9d54
Abhinay yadav <abhinayy48@gmail.com>
2026-01-01 20:37:26 +0530

    Add health check endpoint to server

--- server.js
+++ server.js
@@ -3,10 +3,18 @@ import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import "./services/cron.service.js";
 
+
+
 dotenv.config();
 connectDB();
 
 const app = express();
+
+
+// app.js or server.js
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
 app.listen(process.env.PORT, () =>
   console.log(`Server running on ${process.env.PORT}`)
 );
